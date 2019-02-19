e2ada6bc06941a88d925e350811224c52fcceb28
Liight <Liight@users.noreply.github.com>
2019-02-19 14:07:27 +1300

    Implement initial data management inside a stateful widget
    created a products list to be iterated upon to display dynamic data in a card widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,11 +27,17 @@ class _MyAppState extends State<MyApp> {
                   child: Text('Add Product'),
                 ),
               ),
-              Card(
-                child: Column(children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food Paradise')
-                ]),
+              Column(
+                children: _products
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
               )
             ],
           )),
