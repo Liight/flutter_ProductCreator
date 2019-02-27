86bff5337f4c56ac6f35a5d125d42d439558968e
Liight <Liight@users.noreply.github.com>
2019-02-28 07:10:17 +1300

    Understanding Lifecycle hooks

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -5,10 +5,13 @@ import './products.dart';
 class ProductManager extends StatefulWidget {
   final String startingProduct; // declares immutable string
 
-  ProductManager(this.startingProduct); // binds the startingProduct(above) to this class
+  ProductManager(this.startingProduct){ // binds the startingProduct(above) to this class
+    print('[ProductManager Widget] Constructor');
+  } 
 
   @override // overrides the state of the this class with it's sister state class
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 }
@@ -17,14 +20,22 @@ class _ProductManagerState extends State<ProductManager> {
   List<String> _products = [];
 
   @override // overrides the default initState() method in the current class
-  void initState() { // initialises the state in this class, it will run before the build class reducing the need for a re-render
+  void initState() { // initialises the state in this class, 
+  // it will run before the build class reducing the need for a re-render
+  print('[_ProductManagerState Widget] initState()');
     _products.add(widget.startingProduct); // Add to List widget
     super.initState(); // calls the State (this class extends State) as a constructor before overrides
   }
 
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[_ProductManagerState Widget] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override // overrides the default Widget in this class
   Widget build(BuildContext context) {
+    print('[_ProductManagerState Widget] build()');
     return Column(children: [Container(
       margin: EdgeInsets.all(10.0),
       child: RaisedButton(
--- lib/products.dart
+++ lib/products.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Products extends StatelessWidget {
-  // Absolute black magic. 
-  List<String> products; // Set the product property
-  Products(this.products); // Grabs the incoming 'products' argument and stores it in the products property
+class Products extends StatelessWidget { 
+  final List<String> products; // Set an immutable strongly typed variable
+  Products(this.products){
+    print('[Products Widget] Constructor');
+  } // Grabs the incoming 'products' argument and stores it as a property of the class Products
 
   @override
   Widget build(BuildContext context) {
+    print('[Products Widget] build()');
     return Column(
       children: products
           .map((element) => Card(
                 child: Column(
                   children: <Widget>[
-                    Image.asset('assets/food.jpg'),
+                    // Image.asset('assets/food.jpg'),
                     Text(element)
                   ],
                 ),
