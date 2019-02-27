7016fd9bc0d458fa7f3c5c3b7c0be7b62aae0643
Liight <Liight@users.noreply.github.com>
2019-02-28 06:39:22 +1300

    Passing data to stateful widgets

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -3,16 +3,27 @@ import 'package:flutter/material.dart';
 import './products.dart';
 
 class ProductManager extends StatefulWidget {
-  @override
+  final String startingProduct; // declares immutable string
+
+  ProductManager(this.startingProduct); // binds the startingProduct(above) to this class
+
+  @override // overrides the state of the this class with it's sister state class
   State<StatefulWidget> createState() {
     return _ProductManagerState();
   }
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Tester'];
+  List<String> _products = [];
 
-  @override
+  @override // overrides the default initState() method in the current class
+  void initState() { // initialises the state in this class, it will run before the build class reducing the need for a re-render
+    _products.add(widget.startingProduct); // Add to List widget
+    super.initState(); // calls the State (this class extends State) as a constructor before overrides
+  }
+
+
+  @override // overrides the default Widget in this class
   Widget build(BuildContext context) {
     return Column(children: [Container(
       margin: EdgeInsets.all(10.0),
