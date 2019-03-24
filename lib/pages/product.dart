da4192383b27729470a75b4f112c11a640af394f
Liight <Liight@users.noreply.github.com>
2019-03-25 12:36:44 +1300

    Added product information to the product page.

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -4,32 +4,11 @@ import 'package:flutter/material.dart';
 
 class ProductPage extends StatelessWidget {
   final String title;
+  final String description;
+  final double price;
   final String imageUrl;
 
-  ProductPage(this.title, this.imageUrl);
-
-  _showWarningDialogue(BuildContext context) {
-    showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-              title: Text('Are you sure?'),
-              content: Text('This action cannot be undone'),
-              actions: <Widget>[
-                FlatButton(
-                    child: Text('DISCARD'),
-                    onPressed: () {
-                      Navigator.pop(context); // closes dialoge
-                    }),
-                FlatButton(
-                    child: Text('CONTINUE'),
-                    onPressed: () {
-                      Navigator.pop(context); // closes dialoge
-                      Navigator.pop(context, true); // closes screen
-                    }),
-              ]);
-        });
-  }
+  ProductPage(this.title, this.description, this.price, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
@@ -49,15 +28,48 @@ class ProductPage extends StatelessWidget {
             Image.asset(imageUrl),
             Container(
               padding: EdgeInsets.all(10.0),
-              child: Text(title),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    title,
+                    style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald'),
+                  ),
+                  SizedBox(width: 8.0),
+                  Container(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius: BorderRadius.circular(5.0)),
+                    child: Text(
+                      '\$$price',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
             ),
             Container(
-              padding: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                color: Theme.of(context).accentColor,
-                child: Text('DELETE'),
-                onPressed: () => _showWarningDialogue(context),
+              padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0),
               ),
+              child: Text('Auckland, New Zealand'),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  color: Colors.red,
+                  onPressed: () {},
+                )
+              ],
             )
           ],
         ),
